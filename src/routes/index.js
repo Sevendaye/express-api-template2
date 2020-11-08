fb00212ca17791b8fb80f093a83797417aa20e7d
Dominique <Sevendayes@gmail.com>
2020-11-08 17:37:56 +0100

    Ajout POST/addMessage, addMessage/test

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { indexPage, messagePage } from '../controllers';
+import { indexPage, messagePage, addMessage } from '../controllers';
 
 const indexRouter = express.Router();
 
 indexRouter.get('/', indexPage);
 indexRouter.get('/messages', messagePage);
+indexRouter.post('/messages', addMessage);
 
 export default indexRouter;
