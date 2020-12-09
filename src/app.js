5c107a7b54d34ba5c86256b03820e504fbd5f442
dom <sevendayes&gmail.com>
2020-12-09 17:56:35 +0100

    test gpg commit

--- src/app.js
+++ src/app.js
@@ -1,15 +1,16 @@
-import logger from 'morgan';
-import express from 'express';
-import cookieParser from 'cookie-parser';
-import indexRouter from './routes/index';
+import logger from "morgan";
+import express from "express";
+import cookieParser from "cookie-parser";
+import indexRouter from "./routes/index";
 
+// TEST
 const app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use('/v1', indexRouter);
+app.use("/v1", indexRouter);
 
 app.use((err, req, res, next) => {
   res.status(400).json({ error: err.stack });
